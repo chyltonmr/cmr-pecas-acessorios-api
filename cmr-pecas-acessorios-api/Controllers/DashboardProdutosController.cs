7b7c0c9cab9b638fd30c598c014ab5d1ccb361b4
Chylton M.R <chylton.rezende@hotmail.com>
2025-05-13 21:39:20 -0300

    Update DashboardProdutosController.cs

--- cmr-pecas-acessorios-api/Controllers/DashboardProdutosController.cs
+++ cmr-pecas-acessorios-api/Controllers/DashboardProdutosController.cs
@@ -41,7 +41,7 @@ public class DashboardProdutosController : ControllerBase
     }
 
     [HttpPatch]
-    [Route("produtos/atualizar")]
+    [Route("produto/atualizar")]
     public async Task Update([FromBody] ObterTodosProdutosDto produto)
     {
         //TODO: IMPLEMENTAR
